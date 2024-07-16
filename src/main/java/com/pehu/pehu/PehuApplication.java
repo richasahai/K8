fb6a0c14a8807ff493d68c3e8d406762c4309a97
Richa <sahai.ei@gmail.com>
2024-07-17 01:07:45 +0530

    First Commit

---
+++ src/main/java/com/pehu/pehu/PehuApplication.java
@@ -0,0 +1,13 @@
+package com.pehu.pehu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PehuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PehuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pehu/pehu/PehuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pehu.pehu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PehuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
